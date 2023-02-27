2c90e7c82e08d7fa52f01c6c771b48e886a672f5
ElviraBudna <elvira.budna@gmail.com>
2023-02-27 22:11:44 +0200

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const ref = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  bodyEl: document.body,
+};
+let changeColorTimer = null;
 
+ref.btnStart.addEventListener('click', onClickStart);
+ref.btnStop.addEventListener('click', onClickStop);
+
+function onClickStart(e) {
+  e.currentTarget.disabled = true;
+  changeColorTimer = setInterval(changeColorBody, 1000);
+}
+
+function onClickStop(e) {
+  if (ref.btnStart.disabled) {
+    ref.btnStart.disabled = false;
+    clearTimeout(changeColorTimer);
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColorBody() {
+  ref.bodyEl.style.backgroundColor = getRandomHexColor();
+}
